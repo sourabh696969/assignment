be216cd519c77eb3da4c82054a7f5416cc75fccc
sourabh696969 <sourabhkumar2462@gmail.com>
2024-02-28 18:21:50 +0530

    update

--- Routes/routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.use("/user", require("./userRoute"));
-router.use("/configuration", require("./configurationRoute"));
-
-module.exports = router;
