0e9cfda34bfe955a4427e37cf14be8aafdc8a672
sourabh696969 <sourabhkumar2462@gmail.com>
2024-02-28 18:35:47 +0530

    update

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const connectDB = require("./config/dbConnection");
 const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
+const cors = require("cors");
 
 connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 app.use("/api", require("./Routes/routes"));
 app.use(errorHandler);
