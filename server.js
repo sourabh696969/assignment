9323c47b4b4e8c37bf3bb36c39b8a0c5cd0f049d
sourabh696969 <sourabhkumar2462@gmail.com>
2024-02-29 12:13:00 +0530

    update

--- server.js
+++ server.js
@@ -11,9 +11,6 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.use("/", (req, res) => {
-  res.json({ message: "Welcome to CodeRower Assignment" });
-});
 app.use("/api", require("./routes/routes"));
 app.use(errorHandler);
 
