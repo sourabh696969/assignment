0619f8ccba3cdc294f05eae9a89016705b390490
sourabh696969 <sourabhkumar2462@gmail.com>
2024-02-29 12:06:37 +0530

    update

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/", (req, res) => {
-  res.json({ message: "Welcom to CodeRower Assignment" });
+  res.json({ message: "Welcome to CodeRower Assignment" });
 });
 app.use("/api", require("./routes/routes"));
 app.use(errorHandler);
